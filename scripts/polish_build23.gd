8eeb0df9ba685639f7d547b6d78aba08035058bb
flesentine <chrisgmcc@gmail.com>
2026-08-29 23:10:14 -0700

    Build 23: update drive HUD for conflict matrix

---
+++ scripts/polish_build23.gd
@@ -0,0 +1,8 @@
+extends "res://scripts/polish_build22.gd"
+
+func _update_drive_hud() -> void:
+    super._update_drive_hud()
+    if drive_label != null:
+        drive_label.text = drive_label.text.replace("BUILD 22", "BUILD 23")
+        if not drive_label.text.contains("CONFLICT MATRIX"):
+            drive_label.text += "   CONFLICT MATRIX + PREDICTIVE COPS"
